6349a86b96b774be7961567163c2c3c33d6396ad
Kotzin <kotislife@protnomail.com>
2021-05-19 16:53:09 -0300

    Instance, name and age attribute's created

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,20 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=20):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return f' Olá {id(self)}'
+        return f' Olá'
+
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
+    p = Pessoa('Rafa')
+   # print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
+
+    # usando __init__
+
+    print(p.nome)
+    print(p.idade)
+    p.nome = 'Rafael'
+    print(p.nome)
